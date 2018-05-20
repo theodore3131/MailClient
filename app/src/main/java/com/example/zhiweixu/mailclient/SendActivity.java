434605f6e38b8d18a334929336a859b24ff3c3ad
blackwhite97 <wjz_1997@163.com>
2018-05-20 09:54:58 +0800

    实现了发送界面

--- app/src/main/java/com/example/zhiweixu/mailclient/SendActivity.java
+++ app/src/main/java/com/example/zhiweixu/mailclient/SendActivity.java
@@ -1,14 +1,57 @@
 package com.example.zhiweixu.mailclient;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class SendActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.second_menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-        //假的注视
+
+        // 发送邮件页面的导航栏,把bar加上
+        Toolbar toolbar = findViewById(R.id.send_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // 设置标题
+        toolbar.setTitle("Send Message");
+        //设置logo。您要注意logo与导航位置图标的区别
+//        toolbar.setLogo(R.mipmap.ic_action_send);
+        //添加导航位置图标
+        toolbar.setNavigationIcon(R.mipmap.ic_action_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_send:
+                        Toast.makeText(SendActivity.this, "发送信息", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
